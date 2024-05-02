de5ac6b3544db4b83ab07a1b1fa936b95a67596f
Jason Lantz <jason@muselab.com>
2024-05-02 16:06:52 -0500

    Bypass devhub.sh script and just set up devhub to avoid setting default org

--- d2x_job_run.sh
+++ d2x_job_run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
-/usr/local/bin/devhub.sh
+# Authenticate using Auth URL
+echo "Authenticating to DevHub using auth url..."
+
+# Write the DEV_HUB_AUTH_URL to a file
+echo $DEV_HUB_AUTH_URL > /tmp/dev_hub_auth_url
+
+# Authenticate the DevHub
+sfdx org login sfdx-url -f /tmp/dev_hub_auth_url -a DevHub -d
 
 d2x job run "$@"
